83af786d5155f575b26c0f687adcaa160aa3ee3e
DevMai90 <dmai90@yahoo.com>
2020-01-20 10:36:53 -0800

    Create block style component to filter location

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -4,7 +4,7 @@ export default Component.extend({
   isWide: false,
   actions: {
     toggleImageSize() {
-      // Passing isWide as a string.
+      // Passing isWide as a string. toggle property is a method on the Component object.
       this.toggleProperty('isWide');
     }
   }
