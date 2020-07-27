122db85b5281c09a9b6208c086961d2c092c3245
DevMai90 <dmai90@yahoo.com>
2020-07-26 21:23:36 -0700

    Update integration test

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,26 +1,52 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rental-listing', function(hooks) {
+module('Integration | Component | rental-listing', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  // Mock data that would normally be returned by the route's model hook.
+  hooks.beforeEach(function () {
+    this.rental = {
+      image: 'fake.png',
+      title: 'test-title',
+      owner: 'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    };
+  });
 
-    await render(hbs`{{rental-listing}}`);
+  test('Should display rental details', async function (assert) {
+    assert.expect(6);
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`{{rental-listing rental=this.rental}}`);
 
-    // Template block usage:
-    await render(hbs`
-      {{#rental-listing}}
-        template block text
-      {{/rental-listing}}
-    `);
+    assert.ok(find('.image img'), 'Rental image exists');
+    assert.ok(find('.details h3'), 'Rental title exists');
+    assert.ok(find('.detail.owner'), 'Rental owner exists');
+    assert.ok(find('.detail.type'), 'Rental type exists');
+    assert.ok(find('.detail.location'), 'Rental city exists');
+    assert.ok(find('.detail.bedrooms'), 'Rental bedrooms exists');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('Should toggle wide class on click', async function (assert) {
+    assert.expect(3);
+
+    await render(hbs`{{rental-listing rental=this.rental}}`);
+
+    assert.notOk(find('.wide'), 'Wide class is not set by default');
+
+    await click('.image');
+
+    assert.ok(find('.image.wide'), 'Wide class is toggled after click');
+
+    await click('.image');
+
+    assert.notOk(
+      find('.image.wide'),
+      'Wide class is removed after second click'
+    );
   });
 });
