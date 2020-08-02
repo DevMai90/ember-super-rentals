68f268b927f3c7804cb73a55eddd2b9ddb3558a5
DevMai90 <dmai90@yahoo.com>
2020-08-02 12:40:10 -0700

    Add safeguards to keep filter results in sync with value due to asynchronicity

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -9,16 +9,21 @@ export default Component.extend({
     this._super(...arguments);
     // filter is an Ember method that gets called. Initialized with empty string ''
     // Then set results property according to what is passed into the callback
-    this.filter('').then((results) => this.set('results', results));
+    this.filter('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.value;
+      // filter function is usable by calling the object. This is called closure actions
       let filterAction = this.filter;
-      filterAction(filterInputValue).then((filterResults) =>
-        this.set('results', filterResults)
-      );
+      filterAction(filterInputValue).then((filterResults) => {
+        if (filterResults.query === this.value) {
+          this.set('results', filterResults.results);
+        }
+      });
     }
   }
 });
